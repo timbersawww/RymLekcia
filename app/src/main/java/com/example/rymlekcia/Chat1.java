5c0b0440d69a11f6db523b5fe5c855d82c3011dd
kukuruza939 <kitty.kitty.kk997@gmail.com>
2024-04-01 16:37:07 +0700

    1st commit(selected all files)

---
+++ app/src/main/java/com/example/rymlekcia/Chat1.java
@@ -0,0 +1,14 @@
+package com.example.rymlekcia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chat1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat1);
+    }
+}
